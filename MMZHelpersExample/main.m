b8bdacbba8bf66e0f76c54da3ab5eeffad4a16b5
Hisma Mulya S <mamaz@icehousecorp.com>
2014-10-01 12:12:30 +0700

    Initial Commit

---
+++ MMZHelpersExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MMZHelpersExample
+//
+//  Created by Hisma Mulya S on 10/1/14.
+//  Copyright (c) 2014 mamaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMZAppDelegate class]));
+    }
+}
---
+++ MMZHelpersExampleTests/MMZHelpersExampleTests.m
@@ -0,0 +1,34 @@
+//
+//  MMZHelpersExampleTests.m
+//  MMZHelpersExampleTests
+//
+//  Created by Hisma Mulya S on 10/1/14.
+//  Copyright (c) 2014 mamaz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MMZHelpersExampleTests : XCTestCase
+
+@end
+
+@implementation MMZHelpersExampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
